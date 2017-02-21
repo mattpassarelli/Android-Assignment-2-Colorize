b524ae797e342d63fc16ea548406625369a5535b
Matt Passarelli <mattvpassarelli@gmail.com>
2017-02-21 14:50:51 -0500

    Camera now works. But importing the picture needs work

--- project2_Solution_color/build/generated/source/r/debug/com/library/bitmap_utilities/R.java
+++ project2_Solution_color/build/generated/source/r/debug/com/library/bitmap_utilities/R.java
@@ -15,7 +15,7 @@ public final class R {
 		public static final int ic_launcher = 0x7f020048;
 	}
 	public static final class id {
-		public static final int action_settings = 0x7f0c0068;
+		public static final int action_settings = 0x7f0c006a;
 	}
 	public static final class menu {
 		public static final int main = 0x7f0d0000;
--- project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
+++ project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
@@ -1,19 +1,52 @@
 package com.example.solution_color;
 
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.style.MetricAffectingSpan;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity  {
+import java.io.File;
+import java.io.StringReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView camera;
+    private File external;
+    private File picture;
+    Bitmap imageBitmap;
+    private int width, height;
+    private ImageView background;
+    //private String[] perms = {"android.permission.CAMERA"};
+
+    // @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        // requestPermissions(perms, 200);
+        DisplayMetrics display = this.getResources().getDisplayMetrics();
+        width = display.widthPixels;
+        height = display.heightPixels;
+
+        camera = (ImageView) findViewById(R.id.camera);
+        background = (ImageView) findViewById(R.id.picture);
+        background.setBackgroundResource(R.drawable.gutters);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
@@ -38,7 +71,46 @@ public class MainActivity extends AppCompatActivity  {
         return true;
     }
 
+    public void launchCamera(View view) {
 
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        external = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        picture = new File(external, "temp.jpg");
+
+        camera.putExtra(MediaStore.EXTRA_OUTPUT, picture);
+        startActivityForResult(camera, CONSTANTS.REQUEST_TAKE_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            imageBitmap = (Bitmap) bundle.get("data");
+            Camera_Helpers.saveProcessedImage(imageBitmap, picture.getAbsolutePath());
+            imageBitmap = Camera_Helpers.loadAndScaleImage(picture.getAbsolutePath(), height, width);
+            background = (ImageView) findViewById(R.id.picture);
+            background.setImageBitmap(imageBitmap);
+        } else {
+
+        }
+    }
+
+
+    /*public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults) {
+
+        switch (permsRequestCode) {
+
+            case 200:
+
+                boolean cameraAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                break;
+
+        }
+
+
+    }*/
 
 }
+
 
