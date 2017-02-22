2584d589f1e73ecd4d2484c2fa79b590ca015b82
Matt Passarelli <mattvpassarelli@gmail.com>
2017-02-22 10:24:38 -0500

    Appbar now has translucent grey background. Still need to fix camera problem

--- project2_Solution_color/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ project2_Solution_color/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -238,20 +238,20 @@ public final class R {
 		public static final int abc_config_showMenuShortcutsWhenKeyboardPresent = 0x7f070007;
 	}
 	public static final class color {
-		public static final int abc_background_cache_hint_selector_material_dark = 0x7f0b003d;
-		public static final int abc_background_cache_hint_selector_material_light = 0x7f0b003e;
-		public static final int abc_color_highlight_material = 0x7f0b003f;
+		public static final int abc_background_cache_hint_selector_material_dark = 0x7f0b003e;
+		public static final int abc_background_cache_hint_selector_material_light = 0x7f0b003f;
+		public static final int abc_color_highlight_material = 0x7f0b0040;
 		public static final int abc_input_method_navigation_guard = 0x7f0b0000;
-		public static final int abc_primary_text_disable_only_material_dark = 0x7f0b0040;
-		public static final int abc_primary_text_disable_only_material_light = 0x7f0b0041;
-		public static final int abc_primary_text_material_dark = 0x7f0b0042;
-		public static final int abc_primary_text_material_light = 0x7f0b0043;
-		public static final int abc_search_url_text = 0x7f0b0044;
+		public static final int abc_primary_text_disable_only_material_dark = 0x7f0b0041;
+		public static final int abc_primary_text_disable_only_material_light = 0x7f0b0042;
+		public static final int abc_primary_text_material_dark = 0x7f0b0043;
+		public static final int abc_primary_text_material_light = 0x7f0b0044;
+		public static final int abc_search_url_text = 0x7f0b0045;
 		public static final int abc_search_url_text_normal = 0x7f0b0001;
 		public static final int abc_search_url_text_pressed = 0x7f0b0002;
 		public static final int abc_search_url_text_selected = 0x7f0b0003;
-		public static final int abc_secondary_text_material_dark = 0x7f0b0045;
-		public static final int abc_secondary_text_material_light = 0x7f0b0046;
+		public static final int abc_secondary_text_material_dark = 0x7f0b0046;
+		public static final int abc_secondary_text_material_light = 0x7f0b0047;
 		public static final int accent_material_dark = 0x7f0b0005;
 		public static final int accent_material_light = 0x7f0b0006;
 		public static final int background_floating_material_dark = 0x7f0b0007;
@@ -304,8 +304,8 @@ public final class R {
 		public static final int secondary_text_disabled_material_light = 0x7f0b0038;
 		public static final int switch_thumb_disabled_material_dark = 0x7f0b0039;
 		public static final int switch_thumb_disabled_material_light = 0x7f0b003a;
-		public static final int switch_thumb_material_dark = 0x7f0b0047;
-		public static final int switch_thumb_material_light = 0x7f0b0048;
+		public static final int switch_thumb_material_dark = 0x7f0b0048;
+		public static final int switch_thumb_material_light = 0x7f0b0049;
 		public static final int switch_thumb_normal_material_dark = 0x7f0b003b;
 		public static final int switch_thumb_normal_material_light = 0x7f0b003c;
 	}
--- project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
+++ project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -19,36 +20,40 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.StringReader;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView camera;
+    private Bitmap imageBitmap;
+    private ImageView cameraPic;
     private File external;
     private File picture;
-    Bitmap imageBitmap;
+    private Intent camera;
     private int width, height;
     private ImageView background;
-    //private String[] perms = {"android.permission.CAMERA"};
+    private String[] perms = {"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE"};
 
-    // @TargetApi(Build.VERSION_CODES.M)
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // requestPermissions(perms, 200);
+        requestPermissions(perms, 200);
         DisplayMetrics display = this.getResources().getDisplayMetrics();
         width = display.widthPixels;
         height = display.heightPixels;
 
-        camera = (ImageView) findViewById(R.id.camera);
+        cameraPic = (ImageView) findViewById(R.id.camera);
         background = (ImageView) findViewById(R.id.picture);
         background.setBackgroundResource(R.drawable.gutters);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        myToolbar.getBackground().setAlpha(80);
         setSupportActionBar(myToolbar);
     }
 
@@ -73,43 +78,43 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchCamera(View view) {
 
-        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
         external = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         picture = new File(external, "temp.jpg");
+        Uri file = Uri.fromFile(picture);
 
-        camera.putExtra(MediaStore.EXTRA_OUTPUT, picture);
+        camera.putExtra(MediaStore.EXTRA_OUTPUT, file);
         startActivityForResult(camera, CONSTANTS.REQUEST_TAKE_PHOTO);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
+        if (requestCode == CONSTANTS.REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+
+            //these two lines are my problem
             Bundle bundle = data.getExtras();
             imageBitmap = (Bitmap) bundle.get("data");
+
+
             Camera_Helpers.saveProcessedImage(imageBitmap, picture.getAbsolutePath());
             imageBitmap = Camera_Helpers.loadAndScaleImage(picture.getAbsolutePath(), height, width);
             background = (ImageView) findViewById(R.id.picture);
             background.setImageBitmap(imageBitmap);
         } else {
-
+            Toast.makeText(this, "result code is " + resultCode + " request code is " + requestCode, Toast.LENGTH_LONG).show();
         }
     }
 
-
-    /*public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults) {
+    //I personally need this (I think) because of Nougat's permissions settings
+    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults) {
 
         switch (permsRequestCode) {
-
             case 200:
-
                 boolean cameraAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-
                 break;
-
         }
-
-
-    }*/
+    }
 
 }
 
